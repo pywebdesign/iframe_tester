f6b5de45609f68d553e7cd4441040882f6dda5c1
Pierre-Yves <git@pywebdesign.net>
2016-09-28 16:33:15 -0400

    Create iframe_tester.js

---
+++ iframe_tester.js
@@ -0,0 +1,31 @@
+
+(function(){
+			events = {
+				failed: "iframe_failed",
+				succeed:"iframe_succeed"
+			}
+
+			window.iframeTester = {
+				test: function(url){
+					var iframe = document.createElement('iframe');
+
+					iframe.onload = function() {
+						if(iframe.contentWindow.length == 0){
+							//It failed, do something
+							var event = new Event(events.failed);
+							document.dispatchEvent(event);
+						}else{
+							//it didn't failed
+							var event = new Event(events.succeed);
+							document.dispatchEvent(event);
+						}
+					}; // before setting 'src'
+
+					iframe.src = url;
+					iframe.style.opacity = "0";
+					iframe.style.position = "fixed";
+					//it seams we really need to append the iframe sadly
+					document.body.appendChild(iframe);
+				}
+			}
+		})();
