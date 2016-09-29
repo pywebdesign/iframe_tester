fcabc0318bd1a407af0d9e3d35ad8b0f650a0738
Pierre-Yves <pywebdesign@gmail.com>
2016-09-29 08:22:45 -0400

    prepare for 1.1.0

--- iframe_tester.js
+++ iframe_tester.js
@@ -1,31 +1,30 @@
-
 (function(){
-			events = {
-				failed: "iframe_failed",
-				succeed:"iframe_succeed"
-			}
+	events = {
+		failed: "iframe_failed",
+		succeed:"iframe_succeed"
+	}
 
-			window.IframeTester = {
-				test: function(url){
-					var iframe = document.createElement('iframe');
+	window.IframeTester = {
+		test: function(url){
+			var iframe = document.createElement('iframe');
 
-					iframe.onload = function() {
-						if(iframe.contentWindow.length == 0){
-							//It failed, do something
-							var event = new Event(events.failed);
-							document.dispatchEvent(event);
-						}else{
-							//it didn't failed
-							var event = new Event(events.succeed);
-							document.dispatchEvent(event);
-						}
-					}; // before setting 'src'
-
-					iframe.src = url;
-					iframe.style.opacity = "0";
-					iframe.style.position = "fixed";
-					//it seams we really need to append the iframe sadly
-					document.body.appendChild(iframe);
+			iframe.onload = function() {
+				if(iframe.contentWindow.length == 0){
+					//It failed
+					var event = new Event(events.failed);
+					document.dispatchEvent(event);
+				}else{
+					//it didn't failed
+					var event = new Event(events.succeed);
+					document.dispatchEvent(event);
 				}
-			}
-		})();
+			};
+
+			iframe.src = url;
+			iframe.style.opacity = "0";
+			iframe.style.position = "fixed";
+			
+			document.body.appendChild(iframe);
+		}
+	}
+})();
